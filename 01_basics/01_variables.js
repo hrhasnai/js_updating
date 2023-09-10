d8c2b5ec64647103417ec4846b260c825abae450
Hasnain Rasheed <60227425+hrhasnai@users.noreply.github.com>
2023-09-10 13:52:49 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 6564;
+let accountEmail = "Hasnainhasnai911@gmail.com";
+var accountPassword ="56564";
+city = "Lahore";
+
+// accountId = 6465;
+
+accountEmail = "bc180407212@vu.edu.pk"
+accountPassword = "bc1122";
+city = "Pattoki";
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, city]);
