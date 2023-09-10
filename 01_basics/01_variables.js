a39cadc2454c286f731d33a1dc89ca2bf6777e53
Hasnain Rasheed <60227425+hrhasnai@users.noreply.github.com>
2023-09-10 14:01:42 +0000

    Add comments and accountState

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,12 +2,24 @@ const accountId = 6564;
 let accountEmail = "Hasnainhasnai911@gmail.com";
 var accountPassword ="56564";
 city = "Lahore";
+let accountState ;
 
-// accountId = 6465;
+
+// accountId = 6465 // not allowed beause const value can not to reassign. 
+
+// console.log(accountId);
+
+
 
 accountEmail = "bc180407212@vu.edu.pk"
 accountPassword = "bc1122";
 city = "Pattoki";
 
-// console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([accountId, accountEmail, accountPassword, city]);
